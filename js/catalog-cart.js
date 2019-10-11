e6d873647783ba4ed15c1b2339784f9191d1f3ab
icho-web <54773154+icho-web@users.noreply.github.com>
2019-10-11 21:25:23 +0500

    Правки по критериям

---
+++ js/catalog-cart.js
@@ -0,0 +1,16 @@
+var cartLink = document.querySelectorAll(".buy-1");
+var cartPopup = document.querySelector(".cart-popup");
+var cartClose = document.querySelector(".close-popup");
+var cartColor = document.querySelector(".basket")
+
+for (var i = 0; i < cartLink.length; i++) 
+cartLink[i].addEventListener("click", function(button) {
+  button.preventDefault();
+  cartPopup.classList.add("modal-show");
+  cartColor.classList.add("red");
+});
+
+cartClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.remove("modal-show")
+})
--- js/index-cart.js
+++ js/index-cart.js
@@ -1,11 +1,13 @@
-var cartLink = document.querySelectorAll(".catalog-product button");
+var cartLink = document.querySelectorAll(".buy-1");
 var cartPopup = document.querySelector(".cart-popup");
-var cartClose = document.querySelector(".close-popup");
+var cartClose = document.querySelector(".close-cart-popup");
+var cartColor = document.querySelector(".basket")
 
 for (var i = 0; i < cartLink.length; i++) 
 cartLink[i].addEventListener("click", function(button) {
   button.preventDefault();
-  cartPopup.classList.add("modal-show")
+  cartPopup.classList.add("modal-show");
+  cartColor.classList.add("red");
 });
 
 cartClose.addEventListener("click", function (evt) {
