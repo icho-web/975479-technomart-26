581e56f804bad6f82408d212e7a87b16e758a100
icho-web <54773154+icho-web@users.noreply.github.com>
2019-10-17 00:18:50 +0500

    Правки

--- js/catalog-cart.js
+++ js/catalog-cart.js
@@ -2,7 +2,7 @@ var cartLink = document.querySelectorAll(".buy-1");
 var cartAdd = document.querySelectorAll(".buy-2");
 var cartPopup = document.querySelector(".cart-popup");
 var cartClose = document.querySelector(".close-popup");
-var cartColor = document.querySelector(".basket")
+var cartColor = document.querySelector(".basket");
 
 for (var i = 0; i < cartLink.length; i++) {
   cartLink[i].addEventListener("click", function (evt) {
